df871e1d7bdbe1edeb50d3f9d6df98d7aaded031
Ananta Yudica <nta040689@gmail.com>
2019-12-18 16:14:14 +0700

    Empty Activity Default Generate from android studio

---
+++ app/src/main/java/com/example/android_helloworld_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_helloworld_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
