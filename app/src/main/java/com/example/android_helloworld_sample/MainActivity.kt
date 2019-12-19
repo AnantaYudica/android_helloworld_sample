db2d1b6ce298a118eb3bb352fa41aca90160c2ea
Ananta Yudica <nta040689@gmail.com>
2019-12-19 13:36:06 +0700

    Added code for filled textview in runtime

--- app/src/main/java/com/example/android_helloworld_sample/MainActivity.kt
+++ app/src/main/java/com/example/android_helloworld_sample/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.android_helloworld_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text_view2 = findViewById<TextView>(R.id.text_view_2)
+        text_view2.text = "Hello World!"
+
     }
 }
