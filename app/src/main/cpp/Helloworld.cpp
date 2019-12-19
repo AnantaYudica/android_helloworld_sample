2b0347c40e11445d1643b502263565564a81f85a
Ananta Yudica <nta040689@gmail.com>
2019-12-19 15:57:01 +0700

    Added class Hellowrold Added static member function String for return const string of text "HelloWorld!"

---
+++ app/src/main/cpp/Helloworld.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ananta Yudica on 19/12/2019.
+//
+
+#include "Helloworld.h"
+
+const char * Helloworld::String()
+{
+    return "HelloWorld!";
+}
---
+++ app/src/main/cpp/Helloworld.h
@@ -0,0 +1,15 @@
+//
+// Created by Ananta Yudica on 19/12/2019.
+//
+
+#ifndef ANDROID_HELLOWORLD_SAMPLE_HELLOWORLD_H
+#define ANDROID_HELLOWORLD_SAMPLE_HELLOWORLD_H
+
+class Helloworld {
+public:
+    static const char * String();
+};
+
+
+
+#endif //ANDROID_HELLOWORLD_SAMPLE_HELLOWORLD_H
